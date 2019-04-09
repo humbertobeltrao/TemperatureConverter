36fbcdfa7d6a0e45ab74d83cd2e809e14b8886ec
Humberto Beltrão <humbertobeltrao@gmail.com>
2019-04-08 21:48:27 -0300

    fix method

--- app/src/androidTest/java/com/example/humberto/tempconverter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/humberto/tempconverter/ExampleInstrumentedTest.java
@@ -37,7 +37,9 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
+        assertEquals("com.example.humberto.tempconverter", appContext.getPackageName());
     }
 
     @Rule
