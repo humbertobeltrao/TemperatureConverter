0e708b9545532859c8dba5fe967498cd921b940e
Humberto Beltrão <humbertobeltrao@gmail.com>
2019-04-08 21:42:57 -0300

    updating files

--- app/src/androidTest/java/com/example/humberto/tempconverter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/humberto/tempconverter/ExampleInstrumentedTest.java
@@ -37,9 +37,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.humberto.tempconverter", appContext.getPackageName());
     }
 
     @Rule
