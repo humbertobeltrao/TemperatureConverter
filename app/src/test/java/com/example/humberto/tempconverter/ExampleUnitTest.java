db9eb3e75d0cb81194e09fc27fafedc80cf95235
Humberto Beltrão <humbertobeltrao@gmail.com>
2019-04-08 21:21:11 -0300

    adding unit test method

--- app/src/test/java/com/example/humberto/tempconverter/ExampleUnitTest.java
+++ app/src/test/java/com/example/humberto/tempconverter/ExampleUnitTest.java
@@ -10,7 +10,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
     @Test
     public void celToFah_isCorrect() {
         assertEquals(30, ConverterUtils.celsiusToFahrenheit(0), 0.001);
