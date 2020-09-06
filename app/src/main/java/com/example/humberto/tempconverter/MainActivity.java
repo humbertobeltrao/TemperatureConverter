10efad9d8de79725f82537f3cd923268f679b941
Humberto <humbertobeltrao@gmail.com>
2020-09-06 15:21:25 -0300

    trigger jenkins

--- app/src/main/java/com/example/humberto/tempconverter/MainActivity.java
+++ app/src/main/java/com/example/humberto/tempconverter/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 
     EditText editText;
     TextView textView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
